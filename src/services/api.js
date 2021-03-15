d0330c1f192ec9004fc842efdc5a4228bff7ac5c
Lucio Andrade <lucioandradejr@gmail.com>
2021-03-15 15:53:59 -0400

    App

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+<<<<<<< HEAD
+  baseURL: "http://localhost:3333",
+=======
+  baseURL: "http://192.168.0.23:3333",
+>>>>>>> App
+});
+
+export default api;
